474bfa5ed1f5daeff32fb04708795388ffb61b16
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2023-05-22 14:11:46 +0700

    ok

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: 'e17ec0181ec94b004ac8b506a04ef29c',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
